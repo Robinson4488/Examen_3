4b2672afafc299f4b9d508b025448a29d9d633ec
Robinson Quintero Mesa <robinsonquintero1116452@correo.itm.edu.co>
2024-11-25 09:54:37 -0500

    Update FrmOrdenamiento.java

--- ordenamiento/src/FrmOrdenamiento.java
+++ ordenamiento/src/FrmOrdenamiento.java
@@ -90,7 +90,7 @@ public class FrmOrdenamiento extends JFrame {
         getContentPane().add(spDocumentos, BorderLayout.CENTER);
 
         String nombreArchivo = System.getProperty("user.dir")
-                + "/Examen3-main/ordenamiento/src/datos/Datos.csv";
+                + "/metodo_ordenamiento/ordenamiento/src/datos/Datos.csv";
         Documento.obtenerDatosDesdeArchivo(nombreArchivo);
         Documento.mostrarDatos(tblDocumentos);
 
